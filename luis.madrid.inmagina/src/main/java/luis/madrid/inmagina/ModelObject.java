ede2501abacccc7258eefaee227825a2fdc37248
Luis <Luis@DESKTOP-8P8S1OU>
2017-04-22 18:15:57 +0200

    Original

---
+++ luis.madrid.inmagina/src/main/java/luis/madrid/inmagina/ModelObject.java
@@ -0,0 +1,27 @@
+package luis.madrid.inmagina;
+/**
+* An model for gson demo 
+* 
+* @author ajduke
+*/
+public class ModelObject {
+  String name;
+  int val;
+  boolean status;
+  double f;
+  
+  public ModelObject(String name, int val, 
+  boolean status, double f) {
+    super();
+    this.name = name;
+    this.val = val;
+    this.status = status;
+    this.f = f;
+  }
+  
+  @Override
+  public String toString() {
+    return "ModelObject [name=" + name + ", val=" + val + ", status=" + status + ", f=" + f + "]";
+  }
+
+}
